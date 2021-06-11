73fd3bb40f831a896db853bee0d70b0908de4240
Success0452 <olatundesuccess54@gmail.com>
2021-06-11 16:43:52 +0100

    update

--- app/src/main/java/com/famous/attendancesystem/DiaryActivity.java
+++ app/src/main/java/com/famous/attendancesystem/DiaryActivity.java
@@ -1,14 +1,84 @@
 package com.famous.attendancesystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.List;
 
 public class DiaryActivity extends AppCompatActivity {
+
+    List<Entry> entries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        Toolbar diaryToolbar = findViewById(R.id.diary_toolbar);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView_entries);
+        CalendarView calendarView = findViewById(R.id.calenderView);
+        calendarView.setVisibility(View.GONE);
+
+        DiaryEntryDbHelper dbHelper = new DiaryEntryDbHelper(this);
+        entries = dbHelper.getAllEntries();
+        DiaryAdapter adapter = new DiaryAdapter(entries);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        TabLayout tabLayout = findViewById(R.id.diary_tabLayout_enttries);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                switch (position) {
+                    case 0:
+                        calendarView.setVisibility(View.GONE);
+                        recyclerView.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        recyclerView.setVisibility(View.GONE);
+                        calendarView.setVisibility(View.VISIBLE);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        TextView textView = findViewById(R.id.datetext);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                textView.setText(""+dayOfMonth+"."+month+"."+year);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       
     }
 }
--- app/src/main/java/com/famous/attendancesystem/MainActivity.java
+++ app/src/main/java/com/famous/attendancesystem/MainActivity.java
@@ -1,8 +1,12 @@
 package com.famous.attendancesystem;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +14,39 @@ public class MainActivity extends AppCompatActivity {
     BiometricAct biometricAct;
     Button button_login;
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_login = findViewById(R.id.loginBtn);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
+            @Override
+            public void onClick(View v) {
+                biometricAct.setBiometricPrompt(MainActivity.this);
+            }
+        });
+
+
+        biometricAct = new BiometricAct();
+        if (biometricAct.checkCompatibility(this) == true) {
+
+        }else {
+            alertDialog();
+        }
+    }
+
+    private void alertDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Fingerprint Attendance");
+        builder.setIcon(R.drawable.ic_launcher_background);
+        builder.setMessage("Your device doesn't support fingerprint feature ").setCancelable(false)
+                .setPositiveButton("Exit", ((dialog, which) -> finish()));
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
